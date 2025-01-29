f4f735430337817a37b4587599737bcbed549538
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2025-01-29 11:30:42 +0600

    Update

---
+++ MagicVilla_API/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Data;
+
+public static class VillaStore
+{
+    public static List<VillaDto> villaList = new List<VillaDto>
+    {
+        new VillaDto{Id = 1, Name="Pool View", Occupancy = 1, Sqft = 300},
+        new VillaDto{Id = 2, Name="Milk View", Occupancy = 5, Sqft = 100},
+    };
+}
--- MagicVilla_VillaAPI/MagicVilla_VillaAPI/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MagicVilla_VillaAPI;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
